ae9701b17b51aa3d43354f2f1fd87fcf9ac2360f
EslamAshrafG <eslamashraf113019@gmail.com>
2024-09-17 22:14:16 +0300

    fix bug

--- src/Pages/Alphas/Alphas.jsx
+++ src/Pages/Alphas/Alphas.jsx
@@ -2,36 +2,12 @@
 /* eslint-disable react/prop-types */
 import "../../index.css";
 import React, { useRef } from "react";
-const alphas = [
-  { title: "أ", sound: "../../../public/sounds/ا.mp3" },
-  { title: "ب", sound: "../../../public/sounds/b.mp3" },
-  { title: "ت", sound: "../../../public/sounds/t.mp3" },
-  { title: "ث", sound: "../../../public/sounds/ث.mp3" },
-  { title: "ج", sound: "../../../public/sounds/g.mp3" },
-  { title: "ح", sound: "../../../public/sounds/7.mp3" },
-  { title: "خ", sound: "../../../public/sounds/kh.mp3" },
-  { title: "د", sound: "../../../public/sounds/d.mp3" },
-  { title: "ذ", sound: "../../../public/sounds/th.mp3" },
-  { title: "ر", sound: "../../../public/sounds/r.mp3" },
-  { title: "ز", sound: "../../../public/sounds/z.mp3" },
-  { title: "س", sound: "../../../public/sounds/s.mp3" },
-  { title: "ش", sound: "../../../public/sounds/sh.mp3" },
-  { title: "ص", sound: "../../../public/sounds/ص.mp3" },
-  { title: "ض", sound: "../../../public/sounds/ض.mp3" },
-  { title: "ط", sound: "../../../public/sounds/ط.mp3" },
-  { title: "ظ", sound: "../../../public/sounds/ظ.mp3" },
-  { title: "ع", sound: "../../../public/sounds/3.mp3" },
-  { title: "غ", sound: "../../../public/sounds/gh.mp3" },
-  { title: "ف", sound: "../../../public/sounds/f.mp3" },
-  { title: "ق", sound: "../../../public/sounds/ق.mp3" },
-  { title: "ك", sound: "../../../public/sounds/k.mp3" },
-  { title: "ل", sound: "../../../public/sounds/l.mp3" },
-  { title: "م", sound: "../../../public/sounds/m.mp3" },
-  { title: "ن", sound: "../../../public/sounds/n.mp3" },
-  { title: "ه", sound: "../../../public/sounds/h.mp3" },
-  { title: "و", sound: "../../../public/sounds/wa.mp3" },
-  { title: "ي", sound: "../../../public/sounds/y.mp3" },
-];
+import sounds from "../../assets/sounds/sounds";
+
+const alphas = Object.keys(sounds).map((key) => ({
+  title: key,
+  sound: sounds[key],
+}));
 
 export default function Alphas() {
   return <AlphasItems items={alphas} />;
---
+++ src/assets/sounds/sounds.js
@@ -0,0 +1,61 @@
+import a from "./ا.mp3";
+import b from "./b.mp3";
+import t from "./t.mp3";
+import th from "./ث.mp3";
+import g from "./g.mp3";
+import h7 from "./7.mp3";
+import kh from "./kh.mp3";
+import d from "./d.mp3";
+import th2 from "./th.mp3";
+import r from "./r.mp3";
+import z from "./z.mp3";
+import s from "./s.mp3";
+import sh from "./sh.mp3";
+import s2 from "./ص.mp3";
+import d2 from "./ض.mp3";
+import t2 from "./ط.mp3";
+import th3 from "./ظ.mp3";
+import a3 from "./3.mp3";
+import gh from "./gh.mp3";
+import f from "./f.mp3";
+import q from "./ق.mp3";
+import k from "./k.mp3";
+import l from "./l.mp3";
+import m from "./m.mp3";
+import n from "./n.mp3";
+import h from "./h.mp3";
+import wa from "./wa.mp3";
+import y from "./y.mp3";
+
+const sounds = {
+  أ: a,
+  ب: b,
+  ت: t,
+  ث: th,
+  ج: g,
+  ح: h7,
+  خ: kh,
+  د: d,
+  ذ: th2,
+  ر: r,
+  ز: z,
+  س: s,
+  ش: sh,
+  ص: s2,
+  ض: d2,
+  ط: t2,
+  ظ: th3,
+  ع: a3,
+  غ: gh,
+  ف: f,
+  ق: q,
+  ك: k,
+  ل: l,
+  م: m,
+  ن: n,
+  ه: h,
+  و: wa,
+  ي: y,
+};
+
+export default sounds;
